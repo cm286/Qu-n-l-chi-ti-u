8609e9e68e5418e15ec92157bc38febe757b4c6c
cm286 <manhcong842@gmail.com>
2026-05-30 22:33:15 +0700

    fix cors

--- server/app.js
+++ server/app.js
@@ -3,24 +3,22 @@ const cors = require('cors');
 const dotenv = require('dotenv');
 
 dotenv.config();
+
 const expenseRoute = require('./routes/expenseRoutes');
 const authRoute = require('./routes/authRoute');
 
 const app = express();
+
 app.use(express.json());
-app.use(cors());
+
+app.use(
+    cors({
+        origin: 'https://qu-n-l-chi-ti-u-tau.vercel.app',
+        credentials: true,
+    })
+);
 
 app.use('/api/v2/expense', expenseRoute);
 app.use('/api/v2/auth', authRoute);
 
 module.exports = app;
-const cors = require("cors");
-
-app.use(
-    cors({
-        origin: [
-            "https://qu-n-l-chi-ti-u-tau.vercel.app"
-        ],
-        credentials: true,
-    })
-);
